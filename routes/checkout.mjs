136a0fc5f4be5c3ed2d384d0185c577f043110fa
unknown <enniszeu>
2024-12-17 02:34:30 +0700

    dsds

--- routes/checkout.mjs
+++ routes/checkout.mjs
@@ -30,7 +30,7 @@ router.post("/checkout", async (req, res) => {
       mode: "payment",
       success_url:
         "https://storeeeq-42ca6.web.app/success?session_id={CHECKOUT_SESSION_ID}",
-      cancel_url: "https://storeeeq-42ca6.web.app/cancel",
+      cancel_url: "https://cellvn.com/cancel",
       metadata: {
         email,
       },
